5b1abe2ededa44abf887663992375d64a83f30ee
Peter Downs <github@peterdowns.com>
2023-04-29 15:18:25 -0400

    localias -> v0.0.6

--- update-formula.sh
+++ update-formula.sh
@@ -3,9 +3,11 @@ set -ex
 repo=$1 # peterldowns/nix-search-cli
 recipe=$2 # Formulas/nix-search-cli.rb
 
+mkdir -p tmp
 tag_name=$(curl -X GET -H Accept: application/json https://api.github.com/repos/${repo}/releases/latest | jq -r .tag_name)
 release_url="https://github.com/${repo}/archive/refs/tags/${tag_name}.tar.gz"
-wget "$release_url" -o /dev/null
-release_hash=$(shasum -a 256 "${tag_name}.tar.gz" | cut -d ' ' -f 1)
+wget "$release_url" -O "tmp/${tag_name}.tar.gz" -o /dev/null
+release_hash=$(shasum -a 256 tmp/"${tag_name}.tar.gz" | cut -d ' ' -f 1)
+sed -i '' 's,tag_name = ".*",tag_name = "'"$tag_name"'",g' $recipe
 sed -i '' 's,url ".*",url "'"$release_url"'",g' $recipe
 sed -i '' 's,sha256 ".*",sha256 "'"$release_hash"'",g' $recipe
