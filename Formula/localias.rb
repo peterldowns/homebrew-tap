6aef8ebebf8674d7e1a3b15d0499e8db90bfe1a3
Peter Downs <github@peterdowns.com>
2023-04-29 14:21:15 -0400

    Update localias to v0.0.6

--- Formula/localias.rb
+++ Formula/localias.rb
@@ -1,8 +1,8 @@
 class Localias < Formula
   desc "Manage local domain aliases for devservers"
   homepage "https://github.com/peterldowns/localias"
-  url "https://github.com/peterldowns/localias/archive/refs/tags/release-1677875419-b50c9a4.tar.gz"
-  sha256 "86fa9202281a603d81536b05b75c259aa8f0928ea3d6ed73e015c75fc1ade9e7"
+  url "https://github.com/peterldowns/nix-search-cli/archive/refs/tags/release-1680019370-866017b.tar.gz"
+  sha256 "6cadc554fbc08c45902564f919d01485fd3a6ac8aabd11c74525263c01bb6981"
   license "MIT"
 
   depends_on "go" => :build
@@ -12,7 +12,8 @@ class Localias < Formula
     # https://stackoverflow.com/a/22276273/829926
     # std_go_args definition is here
     # https://github.com/Homebrew/brew/blob/6db7732fa33ab808e405f8ac7673735edd2c8787/Library/Homebrew/formula.rb#L1565
-    system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"localias"), "."
+    ldflags = shell_output("./scripts/golang-ldflags.sh").strip
+    system "go", "build", *std_go_args(ldflags: "-s -w " + ldflags, output: bin/"localias"), "./cmd/localias"
   end
 
   test do
