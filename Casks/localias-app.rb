80af86e502ba5d513bd56fc46aed327a40d73ff3
Peter Downs <github@peterdowns.com>
2023-05-02 21:57:10 -0400

    Rename to localias-app

--- Casks/localias-app.rb
+++ Casks/localias-app.rb
@@ -1,4 +1,4 @@
-cask "localias.app" do
+cask "localias-app" do
   version "0.0.6"
   sha256 "110fa3f4c2953596cffaecd39f2c4d4ded5ba0830518a2df319df3cee5a6e1b7"
   url "https://github.com/peterldowns/localias/releases/download/0.0.6+commit.7c42128/Localias.app.zip"
