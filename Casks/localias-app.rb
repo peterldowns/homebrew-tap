4a09a52d8ae879352e3b4cb9a63e44bbb3ececef
Peter Downs <github@peterdowns.com>
2023-05-19 16:48:54 -0400

    Localias -> 1.0.0+commit.736a4e1

--- Casks/localias-app.rb
+++ Casks/localias-app.rb
@@ -1,7 +1,7 @@
 cask "localias-app" do
-  version "0.0.6"
-  sha256 "110fa3f4c2953596cffaecd39f2c4d4ded5ba0830518a2df319df3cee5a6e1b7"
-  url "https://github.com/peterldowns/localias/releases/download/0.0.6+commit.7c42128/Localias.app.zip"
+  version "1.0.0"
+  sha256 "a8c6985926cb72f1089b64d7f03413861619b6202ced53b860f63e85cbbfc61d"
+  url "https://github.com/peterldowns/localias/releases/download/1.0.0+commit.736a4e1/Localias.app.zip"
   name "Localias.app"
   desc "Localias.app is a menu bar app for developers to securely manage local aliases for development servers."
   homepage "https://github.com/peterldowns/localias"
--- Formula/localias.rb
+++ Formula/localias.rb
@@ -1,25 +1,28 @@
 class Localias < Formula
   desc "Localias is a tool for developers to securely manage local aliases for development servers."
   homepage "https://github.com/peterldowns/localias"
-  url "https://github.com/peterldowns/localias/archive/refs/tags/0.0.7+commit.79e36f7.tar.gz"
-  sha256 "8c8d6a7f310e5669fac44de1db0a9c00ab1cc2012431b9f57fd218283db5690c"
+  url "https://github.com/peterldowns/localias/archive/refs/tags/1.0.0+commit.736a4e1.tar.gz"
+  sha256 "0752f6beec30d89860c4e165b2812091059ccfdc2db93e0c84b7d9efb6205ede"
   license "MIT"
-  version "0.0.7"
+  version "1.0.0"
 
   depends_on "go" => :build
 
   bottle do
-    root_url "https://github.com/peterldowns/localias/releases/download/0.0.7%2Bcommit.79e36f7"
-    sha256 cellar: :any, arm64_monterey: "08929f20d0c99967c54c2c8767476ad3dbe409d011a120488e995f5402ccf78f"
+    root_url "https://github.com/peterldowns/localias/archive/refs/tags/1.0.0+commit.736a4e1.tar.gz"
+    sha256 cellar: :any, arm64_monterey: "2d1f85deda3b0fc856fea6b0c917c57b3a607e54449a7ddef4e6f921559ac344"
+    sha256 cellar: :any, monterey: "5fdf5e93b035d77b683e0e5af78aaf5b2246921220c5938fec240a32ba1648e1"
+    sha256 cellar: :any, x86_64_linux: "c509800b505175d7e4b5c24bc4221e3b131bd35f1a166e65f2e852033faeab78"
+    sha256 cellar: :any, arm64_linux: "ff5703aeef677a808093f6a412ebdbbb45bd0cc0a5fed5771bc7bfc16a604842"
   end
 
   def install
     # Parse the version and commit from the tagref URL because the downloaded
     # .tar.gz isn't a git repository.
-    tag_name = "0.0.7+commit.79e36f7"
-    version = "0.0.7"
-    commit = "79e36f7"
-    ldflags = "-X github.com/peterldowns/localias/cmd/localias/shared.Version=0.0.7 -X github.com/peterldowns/localias/cmd/localias/shared.Commit=79e36f7"
+    tag_name = "1.0.0+commit.736a4e1"
+    version = "1.0.0"
+    commit = "736a4e1"
+    ldflags = "-X github.com/peterldowns/localias/cmd/localias/shared.Version=1.0.0 -X github.com/peterldowns/localias/cmd/localias/shared.Commit=736a4e1"
     # -s -w is standard to make small binaries without debugging information or symbol tables
     # https://stackoverflow.com/a/22276273/829926
     # std_go_args definition is here
