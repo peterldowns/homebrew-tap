cd97325f5d8c8e38d63675d541e9d5d5419f459c
Peter Downs <github@peterdowns.com>
2023-05-19 16:54:09 -0400

    Fix url.

--- Formula/localias.rb
+++ Formula/localias.rb
@@ -9,7 +9,7 @@ class Localias < Formula
   depends_on "go" => :build
 
   bottle do
-    root_url "https://github.com/peterldowns/localias/archive/refs/tags/1.0.0+commit.736a4e1.tar.gz"
+    root_url "https://github.com/peterldowns/localias/releases/download/1.0.0+commit.736a4e1"
     sha256 cellar: :any, arm64_monterey: "2d1f85deda3b0fc856fea6b0c917c57b3a607e54449a7ddef4e6f921559ac344"
     sha256 cellar: :any, monterey: "5fdf5e93b035d77b683e0e5af78aaf5b2246921220c5938fec240a32ba1648e1"
     sha256 cellar: :any, x86_64_linux: "c509800b505175d7e4b5c24bc4221e3b131bd35f1a166e65f2e852033faeab78"
