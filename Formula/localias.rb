170d8ac75a2508a2ff9a6985d5b24e7018460496
Peter Downs <github@peterdowns.com>
2024-03-20 17:37:55 -0400

    localias -> v2.0.2

--- Formula/localias.rb
+++ Formula/localias.rb
@@ -1,36 +1,18 @@
 class Localias < Formula
   desc "Localias is a tool for developers to securely manage local aliases for development servers."
   homepage "https://github.com/peterldowns/localias"
-  url "https://github.com/peterldowns/localias/archive/refs/tags/2.0.1+commit.c358250.tar.gz"
-  sha256 "025bec2dea965e02aa913c60a1907eccc5c7a82644c8cb20693488b78d217dd8"
+  url "https://github.com/peterldowns/localias/archive/refs/tags/2.0.2+commit.56d660e.tar.gz"
+  sha256 "97341409b46f3988054246fa141aaf907d29fe17c3dd2dc980a706bedec1e532"
   license "MIT"
-  version "2.0.1"
+  version "2.0.2"
 
   depends_on "go" => :build
 
   bottle do
-    root_url "https://github.com/peterldowns/localias/releases/download/2.0.1+commit.c358250"
-    sha256 cellar: :any, arm64_monterey: "d356744df1c0de15e897461f54535ef9e2339e4ad021769acbe14d9f72dade23"
-    sha256 cellar: :any, monterey: "a446adb1e8320d8e80fc3edcb3896bc661d34f6628c5abb62d3727881c9f4582"
-    sha256 cellar: :any, x86_64_linux: "cbcf7bfc7b983e28a9eee1516cb65bb3bb7c6d4e178836a6bfdd732ca338638c"
-    sha256 cellar: :any, arm64_linux: "3e58d596512247fffbf4a924040d4eb6215df6cc4556304dc6e577bf0c3c492f"
-  end
-
-  def install
-    # Parse the version and commit from the tagref URL because the downloaded
-    # .tar.gz isn't a git repository.
-    tag_name = "2.0.1+commit.c358250"
-    version = "2.0.1"
-    commit = "c358250"
-    ldflags = "-X github.com/peterldowns/localias/cmd/localias/shared.Version=2.0.1 -X github.com/peterldowns/localias/cmd/localias/shared.Commit=c358250"
-    # -s -w is standard to make small binaries without debugging information or symbol tables
-    # https://stackoverflow.com/a/22276273/829926
-    # std_go_args definition is here
-    # https://github.com/Homebrew/brew/blob/6db7732fa33ab808e405f8ac7673735edd2c8787/Library/Homebrew/formula.rb#L1565
-    system "go", "build", *std_go_args(ldflags: "-s -w " + ldflags, output: bin/"localias"), "./cmd/localias"
-  end
-
-  test do
-    system bin/"localias", "--help"
+    root_url "https://github.com/peterldowns/localias/releases/download/2.0.2+commit.56d660e"
+    sha256 cellar: :any, arm64_monterey: "1d29e7cf03b194a986ccd0d429e7870871d51bc9002c70d13d346bba79250081"
+    sha256 cellar: :any, monterey: "f620c193db0beee7220559143c58222e18d3283b30a95f24abcd0c7fa0e501db"
+    sha256 cellar: :any, x86_64_linux: "e477630875fdec23173063cb4c5dc9b964569f6fcac8914c19d7875bbcc384d4"
+    sha256 cellar: :any, arm64_linux: "ed709a0c74c7e3d243a760808d70228b313975af5384612bfe3b50db811d0896"
   end
 end
