bb1c611ec38156a7a68ccb0665f9e76f0bd9823a
Peter Downs <github@peterdowns.com>
2024-09-23 16:41:02 -0400

    localias -> v2.2.0+commit.d73df9b

--- Formula/localias.rb
+++ Formula/localias.rb
@@ -1,23 +1,23 @@
 class Localias < Formula
   desc "Localias is a tool for developers to securely manage local aliases for development servers."
   homepage "https://github.com/peterldowns/localias"
-  version "2.1.0"
+  version "v2.2.0"
   license "MIT"
 
 
   on_macos do
     # darwin-arm64
     if Hardware::CPU.arm?
-      url "https://github.com/peterldowns/localias/releases/download/2.1.0+commit.fcaac03/localias-darwin-arm64"
-      sha256 "e629d4ea687c381a287f393e124e3c2db2dbc7a710dda2d15a96d0b43d23a1db" # darwin-arm64
+      url "https://github.com/peterldowns/localias/releases/download/v2.2.0+commit.d73df9b/localias-darwin-arm64"
+      sha256 "c95348a170893ac9c95edffc61ab04542001e0abc0f972a09166b14c4520f57e" # darwin-arm64
       def install
         bin.install "localias-darwin-arm64" => "localias"
       end
     end
     # darwin-amd64
     if Hardware::CPU.intel?
-      url "https://github.com/peterldowns/localias/releases/download/2.1.0+commit.fcaac03/localias-darwin-amd64"
-      sha256 "0a3f6d6ffdab28313ae4d17495e506179bb362a10a5c21e48ecbb0a4b967aab0" # darwin-amd64
+      url "https://github.com/peterldowns/localias/releases/download/v2.2.0+commit.d73df9b/localias-darwin-amd64"
+      sha256 "68f0ca33fe7378bc5dc1ccf1eb7dfdf4666cea4d30a6c98b05f9a1ab4653c78b" # darwin-amd64
       def install
         bin.install "localias-darwin-amd64" => "localias"
       end
@@ -26,16 +26,16 @@ class Localias < Formula
   on_linux do
     # linux-arm64
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/peterldowns/localias/releases/download/2.1.0+commit.fcaac03/localias-linux-arm64"
-      sha256 "9915e2bc7e9101c36a20d5f6394fca238a3252c4f588e741025c769acd30846b" # linux-arm64
+      url "https://github.com/peterldowns/localias/releases/download/v2.2.0+commit.d73df9b/localias-linux-arm64"
+      sha256 "6473e5228f15367f016f3d88c0ca48aede6b674fb64ac8b869479a2fa5566105" # linux-arm64
       def install
         bin.install "localias-linux-arm64" => "localias"
       end
     end
     # linux-amd64
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/peterldowns/localias/releases/download/2.1.0+commit.fcaac03/localias-linux-amd64"
-      sha256 "83964c03bbb4a0a026a6c63108f6807d8ec7c9427759a99975a2d14b15f6140c" # linux-amd64
+      url "https://github.com/peterldowns/localias/releases/download/v2.2.0+commit.d73df9b/localias-linux-amd64"
+      sha256 "50c1802c34724c01add0830572a58f091d82eb85acc2bba2f33d02160c265362" # linux-amd64
       def install
         bin.install "localias-linux-amd64" => "localias"
       end
