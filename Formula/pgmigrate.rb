58c8fcdbab44b52621c275f738a7c9b810e5a77e
Peter Downs <github@peterdowns.com>
2024-09-23 16:19:35 -0400

    pgmigrate -> v0.0.7+commit.655bff6

--- Formula/pgmigrate.rb
+++ Formula/pgmigrate.rb
@@ -1,37 +1,47 @@
 class Pgmigrate < Formula
   desc "Pgmigrate is a modern Postgres migrations CLI and library for golang."
   homepage "https://github.com/peterldowns/pgmigrate"
-  url "https://github.com/peterldowns/pgmigrate/archive/refs/tags/v0.0.6+commit.b4febf4.tar.gz"
-  sha256 "a86337989aeb480efdd335219e7d9876408725bedf94b058ea6dedf46410ce1e"
+  version "v0.0.7"
   license "MIT"
-  version "v0.0.6"
 
-  depends_on "go" => :build
-
-  bottle do
-    root_url "https://github.com/peterldowns/pgmigrate/releases/download/v0.0.6+commit.b4febf4"
-    sha256 cellar: :any, arm64_monterey: "fa14ed0c502e1c82d702e1c94af98ce240722c002c3006bb3b07df064c277e20"
-    sha256 cellar: :any, monterey: "8c9ce3d3c54f46a9bf782ad352fd077302ed82e9a19a4917e20120df073efcc0"
-    sha256 cellar: :any, x86_64_linux: "bafebd06639373d9c006795a643d2118dd90a54bd0668fa115b5ad6a6e2170ef"
-    sha256 cellar: :any, arm64_linux: "8442cabf752e2f41301bb93828e9aa26e01f2d025530577a65102595f636cee2"
+  on_macos do
+    # darwin-arm64
+    if Hardware::CPU.arm?
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.0.7+commit.655bff6/pgmigrate-darwin-arm64"
+      sha256 "a2b02fa91c00dfa2a53beeb4fd723181cb56237e3f8ecee571057592c68d2517" # darwin-arm64
+      def install
+        bin.install "pgmigrate-darwin-arm64" => "pgmigrate"
+      end
+    end
+    # darwin-amd64
+    if Hardware::CPU.intel?
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.0.7+commit.655bff6/pgmigrate-darwin-amd64"
+      sha256 "fdb38724efcfa44480a56bc3af94277e3abe3659c6b8d2ee7020be66b4c615e1" # darwin-amd64
+      def install
+        bin.install "pgmigrate-darwin-amd64" => "pgmigrate"
+      end
+    end
   end
-
-  def install
-    # Parse the version and commit from the tagref URL because the downloaded
-    # .tar.gz isn't a git repository.
-    tag_name = "v0.0.6+commit.b4febf4"
-    version = "v0.0.6"
-    commit = "b4febf4"
-    ldflags = "-X github.com/peterldowns/pgmigrate/cli/shared.Version=v0.0.6 -X github.com/peterldowns/pgmigrate/cli/shared.Commit=b4febf4"
-    # -s -w is standard to make small binaries without debugging information or symbol tables
-    # https://stackoverflow.com/a/22276273/829926
-    # std_go_args definition is here
-    # https://github.com/Homebrew/brew/blob/6db7732fa33ab808e405f8ac7673735edd2c8787/Library/Homebrew/formula.rb#L1565
-    system "go", "build", *std_go_args(ldflags: "-s -w " + ldflags, output: bin/"pgmigrate"), "./cli"
+  on_linux do
+    # linux-arm64
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.0.7+commit.655bff6/pgmigrate-linux-arm64"
+      sha256 "99955dcb8cac7bf77b7b8c8fdfe6624542654ca76646591c0ddf35d7b3d572ea" # linux-arm64
+      def install
+        bin.install "pgmigrate-linux-arm64" => "pgmigrate"
+      end
+    end
+    # linux-amd64
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.0.7+commit.655bff6/pgmigrate-linux-amd64"
+      sha256 "9d761ff781f2bc353520ab25ef11f77ce2abf34383c1e1cefe667d909cb4fa0d" # linux-amd64
+      def install
+        bin.install "pgmigrate-linux-amd64" => "pgmigrate"
+      end
+    end
   end
 
   test do
-    system bin/"pgmigrate", "--help"
+    system "#{bin}/pgmigrate --help"
   end
 end
-
