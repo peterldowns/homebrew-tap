3d48e288a10b9df912454042ad6748f4a74d4148
Peter Downs <github@peterdowns.com>
2024-09-24 16:09:42 -0400

    pgmigrate -> v0.1.0+commit.7f13363

--- Formula/pgmigrate.rb
+++ Formula/pgmigrate.rb
@@ -8,16 +8,16 @@ class Pgmigrate < Formula
   on_macos do
     # darwin-arm64
     if Hardware::CPU.arm?
-      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.d9f6b58/pgmigrate-darwin-arm64"
-      sha256 "3914c5692bba3f75f4168d812de0f8406cd57bb31876fc7fc2b6fafe97d6e64d" # darwin-arm64
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.7f13363/pgmigrate-darwin-arm64"
+      sha256 "1e230cc4518de67ba639de7f7351e1a8a7354f418626c3be49103357ea599f0d" # darwin-arm64
       def install
         bin.install "pgmigrate-darwin-arm64" => "pgmigrate"
       end
     end
     # darwin-amd64
     if Hardware::CPU.intel?
-      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.d9f6b58/pgmigrate-darwin-amd64"
-      sha256 "c6bfe1059a88a890185e8128e373f9bd4f4f06d0e3e182eee48718e6eb197787" # darwin-amd64
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.7f13363/pgmigrate-darwin-amd64"
+      sha256 "d0241708d2dce478eacbcf829becd1f6f59347b0481b1e668f69630b1088ece4" # darwin-amd64
       def install
         bin.install "pgmigrate-darwin-amd64" => "pgmigrate"
       end
@@ -26,16 +26,16 @@ class Pgmigrate < Formula
   on_linux do
     # linux-arm64
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.d9f6b58/pgmigrate-linux-arm64"
-      sha256 "5cc1b156a5cbdf1a30c66ad04dd95c1474eeb4ada13755503f49ff59449fae6d" # linux-arm64
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.7f13363/pgmigrate-linux-arm64"
+      sha256 "7877a38688d4d9945c0f16984af7e3def1107ed99f04c1e0807dbe5eb7fb323f" # linux-arm64
       def install
         bin.install "pgmigrate-linux-arm64" => "pgmigrate"
       end
     end
     # linux-amd64
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.d9f6b58/pgmigrate-linux-amd64"
-      sha256 "7bc6d9854e9f483cf013fc0e19343ad839ad63c2463964f5e558bb4310da1c9e" # linux-amd64
+      url "https://github.com/peterldowns/pgmigrate/releases/download/v0.1.0+commit.7f13363/pgmigrate-linux-amd64"
+      sha256 "1538d714cdabe5bc29e7eea6c84784efcf491e842b138155b5657215fc2d911e" # linux-amd64
       def install
         bin.install "pgmigrate-linux-amd64" => "pgmigrate"
       end
